49b781b7c7a19c436b4e84e554de460267440bf4
nocheol <qkdl60@gmail.com>
2024-06-25 12:51:45 +0900

    init

---
+++ qkdl60/problem1/playwright.config.js
@@ -0,0 +1,27 @@
+const {defineConfig, devices} = require("@playwright/test");
+
+module.exports = defineConfig({
+  testDir: "./tests",
+  timeout: 3000,
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 0,
+  workers: 1,
+  use: {
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: {...devices["Desktop Chrome"]},
+    },
+  ],
+  webServer: {
+    command: "npm run test-server",
+    url: "http://127.0.0.1:5500",
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: "http://127.0.0.1:5500",
+  },
+});
---
+++ qkdl60/problem2/base/script.js
@@ -0,0 +1,11 @@
+function addTodo() {
+  // 여기에 기능을 구현하세요.
+}
+
+function deleteTodo(index) {
+  // 여기에 기능을 구현하세요.
+}
+
+function renderTodos() {
+  // 여기에 기능을 구현하세요.
+}
